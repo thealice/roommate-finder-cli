dc979a5762da0a8db40bdd76870e1d6ff1cf3909
Alice Freda <alicefreda@gmail.com>
2019-12-23 23:35:14 +0000

    scraper row attributes

--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -4,12 +4,26 @@ class RoomScraper
     @doc = Nokogiri::HTML(open(index_url))
     binding.pry
   end
+  def call
+    rows.each do |row, doc|
+      scraper_row
+    end
+  end
 
+private
   def rows
     @rows ||= @doc.search("div.content ul.rows p.result-info")
   end
-  # def scrape_time
-  #   @doc.search("p.result-info time")
-  # end
+
+  def scrape_row(row)
+    {
+      :date_created => row.search("time").text,
+      :title => row.search("a.hdrlnk").text,
+      :url => row.search("a.hdrlnk").attribute("href").text,
+      :price => row.search("span.result-price").text
+    }
+  end
+
+
 
 end
