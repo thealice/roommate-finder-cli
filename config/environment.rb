994d7f83453a11f3749f3861ee5d6450cb252ffb
Alice Freda <alicefreda@gmail.com>
2019-12-24 01:04:39 +0000

    add database connection

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,11 @@ Bundler.require
 
 require 'open-uri'
 
+DB = {
+  :connection => SQLite3::Database.new("db/rooms-development.sqlite")
+}
+
+
+binding.pry
+
 require_all 'lib'
--- lib/room.rb
+++ lib/room.rb
@@ -11,12 +11,25 @@ class Room
     room.date_created = hash[:date_created]
     room.price = hash[:price]
     room.url =  hash[:url]
-    binding.pry
     room
   end
 
   def save
-    puts "YOU ARE ABOUT TO SAVE #{self}"
+    # I need a database!!!
+    sql =
+  end
+
+  def self.create_table
+    sql <<-SQL
+      CREATE TABLE IF NOT EXISTS rooms (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        date_created DATETIME,
+        price TEXT,
+        url TEXT
+      )
+    SQL
+    DB[:connection].execute(sql)
   end
 
 end
